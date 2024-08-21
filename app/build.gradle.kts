8a731240b4d47aac1b66c718f15cfc07e1fd7ebd
Captain Throwback <captainthrowback@hotmail.com>
2024-08-21 12:25:09 -0400

    add kpimg 0.11.0-dev to assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,13 +152,6 @@ fun downloadFile(url: String, destFile: File) {
 }
 
 registerDownloadTask(
-    taskName = "downloadKpimg",
-    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kpimg-android",
-    destPath = "${project.projectDir}/src/main/assets/kpimg",
-    project = project
-)
-
-registerDownloadTask(
     taskName = "downloadKptools",
     srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kptools-android",
     destPath = "${project.projectDir}/libs/arm64-v8a/libkptools.so",
@@ -185,7 +178,6 @@ tasks.register<Copy>("mergeFlashableScript") {
 }
 
 tasks.getByName("preBuild").dependsOn(
-    "downloadKpimg",
     "downloadKptools",
     "downloadCompatKpatch",
     "mergeFlashableScript",
